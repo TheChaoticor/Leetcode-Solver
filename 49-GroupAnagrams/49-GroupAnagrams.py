6abc4ddebd75f8e542026f6695a532623874b9e6
Arpit Mishra <113346036+TheChaoticor@users.noreply.github.com>
2025-04-06 08:36:42 +0530

    File updated at 4/6/2025, 8:36:41 AM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,11 +1,14 @@
-# Last updated: 4/6/2025, 7:51:02 AM
+# Last updated: 4/6/2025, 8:36:41 AM
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ls=defaultdict(list)
-        for i in strs:
-            l="".join(sorted(i))
-            ls[l].append(i)
-        return list(ls.values())
-
-               
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic=Counter(nums)
+        dic=dict(sorted(dic.items(),key= lambda item:item[1],reverse=True))
         
+        ls=[]
+        c=0
+        for i in dic:
+            if c==k:
+                break
+            ls.append(i)
+            c+=1
+        return ls        
