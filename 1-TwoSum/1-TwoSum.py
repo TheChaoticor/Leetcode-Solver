85d5a6b99868065b285b0b8f38cfc49a58504fb6
Arpit Mishra <113346036+TheChaoticor@users.noreply.github.com>
2025-04-08 23:02:00 +0530

    File updated at 4/8/2025, 11:01:59 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,15 @@
+# Last updated: 4/8/2025, 11:01:59 PM
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sum=[]
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return[i,j]
-                    
-                   
-        return []            
+    def longestConsecutive(self, nums: List[int]) -> int:
+        ls=set(nums)
+        lolng=0
+        for num in ls:
+            if num-1 not in ls:
+                curr=num
+                c=1
+                while curr+1 in ls:
+                    c+=1
+                    curr+=1
+                lolng=max(lolng,c)
 
-
+        return lolng            
